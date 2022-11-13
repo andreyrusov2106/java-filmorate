d6b4056b1a0f35883fe02b106761198d1b172c61
Ovabor <ovabor1991@gmail.com>
2022-11-13 19:13:48 +0300

    add-rec

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -7,7 +7,6 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.filmorate.exceptions.ResourceNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.storage.director.DirectorFilmDbStorage;
@@ -178,11 +177,7 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public Film getFilm(Long id) {
-        try {
-            return jdbcTemplate.queryForObject(SELECT_FILM_BY_ID_SQL, this::mapRowToFilm, id);
-        } catch(EmptyResultDataAccessException e) {
-            throw new ResourceNotFoundException("Film with id=" + id + " not found");
-        }
+        return jdbcTemplate.queryForObject(SELECT_FILM_BY_ID_SQL, this::mapRowToFilm, id);
     }
 
     @Override
